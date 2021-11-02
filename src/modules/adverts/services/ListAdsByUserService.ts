a0c232879fe5e0462939df6a56f220a9129602d9
JDanrley <jose.silva478@fatec.sp.gov.br>
2021-11-02 11:42:54 -0300

    retorno no anuncio padronizado

--- src/modules/adverts/services/ListAdsByUserService.ts
+++ src/modules/adverts/services/ListAdsByUserService.ts
@@ -6,6 +6,7 @@ import ICarsRepository from '../repositories/ICarsRepository';
 import IAdsRepository from '../repositories/IAdsRepository';
 import IVehicleItemsRepository from '../repositories/IVehicleItemsRepository';
 import Ad from '../../adverts/infra/typeorm/entities/Ad';
+import User from '@modules/users/infra/typeorm/entities/User';
 
 @injectable()
 class ListAdsByUserService {
@@ -16,6 +17,10 @@ class ListAdsByUserService {
   ) {}
   public async execute(user_id: string): Promise<Ad[]> {
     const ads = await this.adsRepository.findAllByUser(user_id)
+
+    ads.forEach(ad => {
+      ad.user = new User();
+    });
 
     return ads;
   }
