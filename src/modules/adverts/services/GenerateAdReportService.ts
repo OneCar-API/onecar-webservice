2c9a77f9a4b66eb850359a31ff6942ce8cc6abb0
raquelribeiro2 <raqu3l.rodriguesribeiro@gmail.com>
2021-11-26 22:31:14 -0300

    Relatórios dias online, visualizações e interesses

--- src/modules/adverts/services/GenerateAdReportService.ts
+++ src/modules/adverts/services/GenerateAdReportService.ts
@@ -1,26 +1,27 @@
 import path from 'path';
 import { injectable, inject } from 'tsyringe';
+import { differenceInDays } from 'date-fns';
 
 import AppError from '@shared/errors/AppError';
-import IUsersRepository from '../repositories/IUsersRepository';
+import IUsersRepository from '@modules/users/repositories/IUsersRepository';
 
-import IHashProvider from '../providers/HashProvider/models/IHashProvider';
+import IHashProvider from '@modules/users/providers/HashProvider/models/IHashProvider';
 
-import User from '../infra/typeorm/entities/User';
-import IUsersTokensRepository from '../repositories/IUsersTokensRepository';
+import IUsersTokensRepository from '@modules/users/repositories/IUsersTokensRepository';
 import IMailProvider from '@shared/container/providers/MailProvider/models/IMailProvider';
 import IAdsRepository from '../repositories/IAdsRepository';
 
 interface IRequest {
-  name: string;
-  nickname: string;
-  document: string;
-  cnpj?: string;
-  email: string;
-  password: string;
-  phone?: string;
+  user_id: string;
+  ad_id: string;
 }
 
+interface IResponse {
+  ad_id: string;
+  time: number,
+  interests: number,
+  views: number,
+}
 
 @injectable()
 class GenerateAdReportService {
@@ -31,76 +32,72 @@ class GenerateAdReportService {
     @inject('AdsRepository')
     private adsRepository: IAdsRepository,
 
-    @inject('UsersTokensRepository')
-    private usersTokensRepository: IUsersTokensRepository,
-
-    @inject('HashProvider')
-    private hashProvider: IHashProvider,
-
     @inject('MailProvider')
     private mailProvider: IMailProvider,
   ) {}
 
   public async execute({
-      name,
-      nickname,
-      document,
-      cnpj,
-      email,
-      phone,
-      password,
-    }: IRequest): Promise<User> {
-    const checkUserExists = await this.usersRepository.findByEmail(email);
+      user_id,
+      ad_id,
+    }: IRequest): Promise<IResponse> {
+    const user = await this.usersRepository.findById(user_id);
 
-    if (checkUserExists) {
-      throw new AppError('Email address already in use.', 401);
+    if (!user) {
+      throw new AppError('User not found', 404)
     }
 
-    const checkNicknameExists = await this.usersRepository.findByNickname(nickname);
+    const ad = await this.adsRepository.findById(ad_id);
 
-    if (checkNicknameExists) {
-      throw new AppError('Nickname already in use.', 401);
+    if (!ad) {
+      throw new AppError('Ad not found.', 404);
     }
 
-    const hashedPassword = await this.hashProvider.generateHash(password);
-
-    if (phone) {
-      const isNumber = phone.match(/^\d+$/) !== null;
-
-      if (!isNumber) {
-        throw new AppError('Phone formatting is wrong.', 401);
-      }
+    if (ad.user_id !== user.id) {
+      throw new AppError('This ad does not belong to you.', 403);
     }
 
-    if (document) {
-      const isNumber = document.match(/^\d+$/) !== null;
+    const getAdCreatedDate = ad.created_at;
 
-      if (!isNumber) {
-        throw new AppError('Document formatting is wrong.', 401);
-      }
+    const onlineAdTime = differenceInDays(Date.now(), getAdCreatedDate);
+
+
+    const averageOfInterestsPerDay = Math.round(ad.interests / onlineAdTime);
+
+    const averageViewsPerDay = Math.round(ad.views / onlineAdTime);
+
+    const report = {
+      ad_id: ad.id,
+      time: onlineAdTime,
+      interests: averageOfInterestsPerDay,
+      views: averageViewsPerDay,
     }
 
-    if (cnpj) {
-      const isNumber = cnpj.match(/^\d+$/) !== null;
+    const reportAd = path.resolve(
+      __dirname,
+      '..',
+      'views',
+      'report.hbs',
+    );
 
-      if (!isNumber) {
-        throw new AppError('CNPJ formatting is wrong.', 401);
-      }
-    }
-
-    const user = await this.usersRepository.create({
-      name,
-      nickname,
-      document,
-      cnpj,
-      email,
-      password: hashedPassword,
-      phone,
-      is_legal: cnpj ? true : false,
-      is_active: false,
+    await this.mailProvider.sendMail({
+      to: {
+        name: user.name,
+        email: user.email,
+      },
+      subject: '[OneCar] Relatório do Anúncio',
+      templateData: {
+        file: reportAd,
+        variables: {
+          name: user.name,
+          time: report.time,
+          interests: report.interests,
+          views: report.views,
+        },
+      },
     });
 
-    return user;
+    return report;
+
   }
 }
 
